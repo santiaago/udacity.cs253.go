c9fa36ff11a1064787d4a209a8a61f73d51dcca7
Santiaago <santiago.ariassar@gmail.com>
2013-06-09 23:01:17 +0200

    unit2 part 2

--- main.go
+++ main.go
@@ -1,26 +1,20 @@
 package main
 
 import (
-	"appengine"
-	// "appengine/datastore"
-	// "appengine/user"
-	"html/template"
-	"net/http"
 	"fmt"
-	"io"
-	// "time"
+	"net/http"
+	"appengine"
+	"unit1"
+	"unit2"
 )
-
-type Date struct{
-	Month string
-	Day string
-	Year string
-	Error string
-}
 
 func init(){
 	http.HandleFunc("/", mainHandler)
-	http.HandleFunc("/unit1/date", dateHandler)
+	http.HandleFunc("/unit1/date", unit1.DateHandler)
+	http.HandleFunc("/unit1/thanks", unit1.ThanksHandler)
+	http.HandleFunc("/unit2/rot13", unit2.Rot13Handler)
+	http.HandleFunc("/unit2/signup", unit2.SignupHandler)
+	http.HandleFunc("/unit2/welcome", unit2.WelcomeHandler)
 }
 
 func mainHandler(w http.ResponseWriter, r *http.Request){
@@ -28,68 +22,7 @@ func mainHandler(w http.ResponseWriter, r *http.Request){
 	c.Infof("Requested URL: %v", r.URL)
 	
 	fmt.Fprint(w,"hello Udacity with Go!")
-}
-
-var dateTemplate = template.Must(template.New("MyDate").Parse(dateHTML))
-
-const dateHTML = `
-<html>
-<body>
-<form method="post">
-   What is your birthday?
-   <br>
-	<label>Month
-   	<input name="month" value="{{.Month}}">
-   </label>
-	<label>Day
-		<input name="day" value="{{.Day}}">
-   </label>
-	<label>Year
-		<input name="year" value="{{.Year}}">
-   </label>
-	<div style="color:red">{{.Error}}</div>
-	<br>
-   <br>
-   <input type="submit">
-</form>
-</body>
-</html>
-`
-
-func dateHandler(w http.ResponseWriter, r *http.Request){
-	c := appengine.NewContext(r)
-	c.Infof("cs253: Requested URL: %v", r.URL)
-	c.Infof("cs253: Http METHOD: %v",r.Method)
-	if r.Method == "GET" {
-		date := Date{
-			Month: "",
-			Day: "",
-			Year: "",
-		}
-		
-		if err := dateTemplate.Execute(w,date); err != nil{
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-	} else if r.Method == "POST"{
-		d := Date{
-			Month: r.FormValue("month"),
-			Day: r.FormValue("day"),
-			Year: r.FormValue("year"),			
-		}
-		fmt.Fprint(w, "Month:",d.Month," Day:",d.Day," Year:",d.Year)
-	}else{
-		error404(w)
-		return
-	}
-}
-
-func error404(w http.ResponseWriter) {
-	w.WriteHeader(http.StatusNotFound)
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	io.WriteString(w, "404: Not Found")
-}
- 
+} 
 
 
 
